54306608b52caa3b090f7f1ee0136a7b313d0d4e
Gints Stikans <gints.stikans@vaimo.com>
2023-03-17 08:16:26 +0200

    Jenkins

--- commands/jenkins.js
+++ commands/jenkins.js
@@ -14,7 +14,6 @@ const projectBuild = async (projectName, branch) => {
     let configData = await readAndValidateConfig()
 
     if (typeof configData.token !== "undefined") {
-
         // const jr = https.request({
         //     hostname: jenkinsUrl,
         //     path: `/job/project_${projectName}/buildWithParameters?BRANCH=${branch}`,
@@ -25,27 +24,6 @@ const projectBuild = async (projectName, branch) => {
         //     jres.on(data, chunk => {})
         // })
         // jr.write();
-        https.get('https://jsonplaceholder.typicode.com/users', res => {
-  let data = [];
-  const headerDate = res.headers && res.headers.date ? res.headers.date : 'no response date';
-  console.log('Status Code:', res.statusCode);
-  console.log('Date in Response header:', headerDate);
-
-  res.on('data', chunk => {
-    data.push(chunk);
-  });
-
-  res.on('end', () => {
-    console.log('Response ended: ');
-    const users = JSON.parse(Buffer.concat(data).toString());
-
-    for(user of users) {
-      console.log(`Got user with id: ${user.id}, name: ${user.name}`);
-    }
-  });
-}).on('error', err => {
-  console.log('Error: ', err.message);
-});
     }
 }
 
