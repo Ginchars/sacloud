197d159ecd959a1bd21d9d290c2e21b8a3928fdd
Gints Stikans <gints.stikans@vaimo.com>
2023-03-17 10:04:00 +0200

    Updated usage info

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 #! /usr/bin/env node
 const { program } = require('commander')
+const chalk = require('chalk')
 const { dbpass, createDbDump } = require('./commands/db')
 const { projectPods, sshProjectPods } = require('./commands/pods')
 const { executeCliCommand } = require('./commands/mcli')
@@ -11,6 +12,7 @@ process.on('SIGINT', function() {
 program
     .command('db-pass')
     .arguments('<projectName>')
+    .usage(`${chalk.green('<projectName>')}`)
     .description('Get DB Pass')
     .action(dbpass);
 
@@ -19,6 +21,8 @@ program
     .arguments('<projectName>')
     .option('-f, --full-dump', 'Do full DB dump')
     .option('-n, --new-dump', 'Create a new dump file even if there is one')
+    .usage(`${chalk.green('<projectName>')} [options -f|--full-dump -n|--new-dump]`)
+    .combineFlagAndOptionalValue(true)
     .description('Get DB Dump')
     .action(createDbDump);
 
@@ -26,6 +30,7 @@ program
     .command('pod-list')
     .arguments('<projectName>')
     .option('-t, --type <type>', 'Get Specific Pods')
+    .usage(`${chalk.green('<projectName>')} [options -t|--type <type>]`)
     .description('Get Project Pod List')
     .action(projectPods);
 
@@ -33,13 +38,15 @@ program
     .command('ssh-pods')
     .arguments('<projectName>')
     .option('-t, --type <type>', 'SSH In specific project pods', 'web')
-    .description('SSH in project pods')
+    .usage(`${chalk.green('<projectName>')} [options -t|--type <type>|default:web]`)
+    .description('SSH in projects pods - defaults to web')
     .action(sshProjectPods);
 
 program
     .command('m-cli')
     .arguments('<projectName>')
     .option('-c, --command <command>', 'Magento CLI command')
+    .usage(`${chalk.green('<projectName>')} [options -c|--command <command>]`)
     .description('Execute Magento CLI command on project cron pod')
     .action(executeCliCommand);
 
